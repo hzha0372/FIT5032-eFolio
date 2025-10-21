63a54424c0b3bf9dabf461b9553766c1049c1297
zhanghongrui <hzha0372@student.monash.edu>
2025-10-21 14:15:07 +0800

    week12

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
+/* global process */
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(), 
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032-eFolio/'
+    : '/',
   define: {
     'process.env': {
-      BASE_URL: '/', 
-    },
+      BASE_URL: process.env.NODE_ENV === 'production'
+        ? '/FIT5032-eFolio/'
+        : '/'
+    }
   },
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  }
 })
