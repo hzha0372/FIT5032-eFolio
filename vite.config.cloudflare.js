4f824d02e89c7e2bc0b34d6e5c35e87f70711c0a
zhanghongrui <hzha0372@student.monash.edu>
2025-10-21 14:38:58 +0800

    week12

---
+++ vite.config.cloudflare.js
@@ -0,0 +1,12 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  base: '/',  
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  }
+})
